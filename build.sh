5c7ce689db1620bb027476fb71132e99a76ea1be
marcus.zhang <marcuszhang@augmentum.com.cn>
2020-10-03 22:10:23 +0800

    feat(travis): test upload by ecs

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+COS_SECRET_ID=$1
+COS_SECRET_KEY=$2
+COS_BUCKET_NAME=$3
+COS_BUCKET_REGION=$4
+rm -rf ./demo-frontend/
+tar -xzvf build.tar.gz
+coscmd config -a $COS_SECRET_ID -s $COS_SECRET_KEY -b $COS_BUCKET_NAME -r $COS_BUCKET_REGION
+coscmd upload -r ./demo-frontend/ /
+printf 'done.\n'
