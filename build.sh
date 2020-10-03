1015410b37f233c4f7e798743e79ed2c79e05975
marcus.zhang <marcuszhang@augmentum.com.cn>
2020-10-03 22:17:34 +0800

    feat(travis): param path

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ COS_SECRET_ID=$1
 COS_SECRET_KEY=$2
 COS_BUCKET_NAME=$3
 COS_BUCKET_REGION=$4
+FOLDER_PATH=$5
+cd $FOLDER_PATH
 rm -rf ./demo-frontend/
 tar -xzvf build.tar.gz
 coscmd config -a $COS_SECRET_ID -s $COS_SECRET_KEY -b $COS_BUCKET_NAME -r $COS_BUCKET_REGION
