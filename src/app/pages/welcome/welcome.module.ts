eb543028da2c797baa052351106443f3d3049246
marcus.zhang <marcuszhang@augmentum.com.cn>
2020-12-09 19:32:02 +0800

    temp

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { NzTableModule } from 'ng-zorro-antd/table';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome.component';
 
 @NgModule({
-  imports: [WelcomeRoutingModule, SharedModule],
+  imports: [WelcomeRoutingModule, SharedModule, NzTableModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent],
 })
